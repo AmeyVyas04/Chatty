36f6a1448af57755c20e9084b13210d273e10564
AmeyVyas04 <amey040176@gmail.com>
2025-03-28 12:21:16 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,7 @@ let port = process.env.PORT
 
 app.use(
     cors({
-      origin: "http://localhost:5173",
+      origin: "https://chatty1-delta.vercel.app/",
       credentials: true,
     })
   );
