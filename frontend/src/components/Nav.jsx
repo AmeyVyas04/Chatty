0510386b11f5420edfbd26459e760b62cbf03f08
AmeyVyas04 <amey040176@gmail.com>
2025-03-28 14:18:48 +0530

    Update Nav.jsx

--- frontend/src/components/Nav.jsx
+++ frontend/src/components/Nav.jsx
@@ -27,12 +27,12 @@ function Nav() {
       <ul
         tabIndex={0}
         className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
-        <li>
-          <a className="justify-between" href='/profile'>
-            Profile
-            <span className="badge">New</span>
-          </a>
-        </li>
+       <li>
+  <Link to="/profile" className="justify-between">
+    Profile
+    <span className="badge">New</span>
+  </Link>
+</li>
         <li><Link to="/setting">Settings</Link></li>
         <li>
           <LogoutButton /> {/* ✅ Just use it, no props needed */}
